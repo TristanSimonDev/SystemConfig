2454ba07104f636fa954accb07f8c5aff11da3d1
TristanSquared <simontristan7766@gmail.com>
2024-05-24 22:58:05 +0200

    fix for backround on Windows 11!

--- src/Tools/System/Windows11/Backround.bat
+++ src/Tools/System/Windows11/Backround.bat
@@ -3,4 +3,7 @@ cls
 echo %cd%
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%cd%\src\IMG\Windows11.jpg" /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
+taskkill /f /im explorer.exe
+start explorer.exe
 pause
