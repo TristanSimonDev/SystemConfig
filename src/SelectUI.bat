243be32c0a0b2abb04c590f4eafa09c98f233123
TristanSquared <simontristan7766@gmail.com>
2024-05-25 07:52:18 +0200

    done for now

--- src/SelectUI.bat
+++ src/SelectUI.bat
@@ -1,15 +1,12 @@
 @echo off
 :start
 cls
-echo %cd%
 cd %cd%
 color e
 setlocal enabledelayedexpansion
 
 echo (1) = DeleteSelector
-echo (2) = InternetSelector
-echo (3) = ProgrammSelector
-echo (4) = Change System Settings (Under review)
+echo (2) = Change System Settings
 
 set /p UI=What Branche do you want to choose: 
 if not defined UI goto :start
@@ -19,14 +16,6 @@ if "%UI%"=="1" (
 )
 
 if "%UI%"=="2" (
-    call src\Content\InternetUI.bat
-)
-
-if "%UI%"=="3" (
-    call src\Content\ProgrammUI.bat
-)
-
-if "%UI%"=="4" (
     call src\Content\SystemConfigUI.bat
 )
 
